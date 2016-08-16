7bf3067d630fcfabc176bfd0e3abbb6fc82d2129
Vitor Fernandes <vitorfsrrg@hotmail.com>
2016-08-16 10:21:14 -0300

    final de aula

---
+++ aula20160816/mag.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+  if(arg == 1)
+         printf("Bom dia, mundo!\n");
+  
+  else
+         printf("Bom dia, %s!\n", argv[1]);
+  return 0;
+}
